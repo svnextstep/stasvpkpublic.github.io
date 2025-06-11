44d7969257b4d12792d724898ebd7a237854eb10
svnextstep <svetlanakarpuk@gmail.com>
2025-06-11 13:31:53 +0300

    correct lenses page

---
+++ resources/views/components/grid2.blade.php
@@ -0,0 +1,3 @@
+<div class="grid-2 @isset ($sClass) {{ $sClass }}" @endisset>
+  {{ $slot }}
+</div>
--- resources/views/tvsystem/lenses.blade.php
+++ resources/views/tvsystem/lenses.blade.php
@@ -56,21 +56,36 @@
   @foreach ($aLenses as $iId => $aModel)
     @php $sActive = ($loop->index == 0) ? '_active' : '' @endphp
     
-    <x-tabs-content-item sClass="lenses-tab__content lenses-tab__{{$iId}}  {{$sActive}}" >
-      <x-lenses-focus></x-lenses-focus>
+    <x-tabs-content-item sClass="tab-lens lenses-tab__{{$iId}}  {{$sActive}}" >
       
-      <x-params-list sClass='_active'>
-        
-        @foreach ($aModel['params'] as $sParamName => $aParam)
-          <x-params-item sName="{{ __('messages.' . $sParamName) }}">{{$aParam['value']}} {{  __('messages.' . $aParam['unit']) }}</x-params-item>
-        @endforeach
-        
-        <x-params-item sName="используются в телесистемах">
-          <h3 class="_color-accent">{{ __('messages.text_vpk_name') . " " . $aModel['used_in'] }}</h3>
-        </x-params-item>
-        
-      </x-params-list> 
+      <div class="tab-lens__focus">
+        @for ($i = 1; $i <= 3; $i++)
+        <div @if ( $i  == $iId)class="_active-win" @endif></div>
+        @endfor
+      </div>
       
+      <div class="tab-lens__params lens-params__{{$iId}} _active">
+        @for ($i = 1; $i <= 2; $i++)
+        <div>
+          @if (($iId == 1 && $i != 1) || ($iId != 1 && $i == 1))
+            @php $direction = ($iId != 1) ? "_direction-right" : ""; @endphp
+            
+            <x-params-list  sClass="{{$direction}}"  >
+
+              @foreach ($aModel['params'] as $sParamName => $aParam)
+                <x-params-item sName="{{ __('messages.' . $sParamName) }}">{{$aParam['value']}} {{  __('messages.' . $aParam['unit']) }}</x-params-item>
+              @endforeach
+
+              <x-params-item sName="используются в телесистемах">
+                <h3 class="_color-accent">{{ __('messages.text_vpk_name') . " " . $aModel['used_in'] }}</h3>
+              </x-params-item>
+
+            </x-params-list>
+          @endif
+        </div>
+        @endfor
+      </div>  
+
     </x-tabs-content-item>
   @endforeach
   
@@ -123,93 +138,95 @@
   <h2>О чем говорят эти характеристики на практике?</h2>
   
   {{-- AGRUMENT 1 --}}
-  <x-row>
+  <x-row sClass="lenses-practice">
     
     <x-item-center>
-      <p>Лесной массив можно осматривать любыми системами, вопрос в результате этого осмотра. Возможно ли рассмотреть возгорание на нечетком изображении, где все объекты и цвета сливаются в единое неразборчивое пятно?</p>
-      <p>Выбор очевиден и он в пользу <span class="_color-accent">специализированных телесистем ВПК</span>.</p>
-
-    </x-item-center>
+      <div class="list-big-number">
+        <div class="list-big-number__bg">01</div>
+        <p>Лесной массив можно осматривать любыми системами, вопрос в результате этого осмотра. Возможно ли рассмотреть возгорание на нечетком изображении, где все объекты и цвета сливаются в единое неразборчивое пятно?</p>
+        <p>Выбор очевиден и он в пользу <span class="_color-accent">специализированных телесистем ВПК</span>.</p>
+      </div>
+      </x-item-center>
     
-    <x-item sClass='forest-view'>
-      <div class='forest-view__bg'></div>
+    <x-item>
       
-      <div class='forest-view__compare-img-list'>
-        <x-item-no-padding>
+      <x-grid2 sClass="forest-view">
+        <div class="forest-view__bg grid-unite-row-cells">
+          <img src="{{ asset('/img/tv_system/lenses/compare_origin_view.jpg') }}"  width="240" height="auto" alt="" loading='lazy'/>
+        </div>
+
+        <div class="forest-view__compare-img-list">
           <img src="{{ asset('/img/tv_system/lenses/compare_high.jpg') }}" width="240" height="auto" alt="" loading='lazy'/>
           <p><b>телесистемы ВПК</b></p>
-        </x-item-no-padding>
+        </div>
 
-        <x-item-no-padding>
+        <div class="forest-view__compare-img-list">
           <img src="{{ asset('/img/tv_system/lenses/compare_low.jpg') }}"  width="240" height="auto" alt="" loading='lazy'/>
           <p><b>бытовые системы</b></p>
-        </x-item-no-padding>
-      </div>
+        </div>
+      </x-grid2>
+      
     </x-item>
     
   </x-row>
-  
+
   {{-- AGRUMENT 2 --}}
-  <x-row sClass='lenses-compare'>
+  <x-row sClass="mobile-row-reverse lenses-practice">
     <x-item>
-      
-      <x-row>
+      <x-grid2  sClass='lenses-compare '>
         {{-- cameras 1 --}}
-        <x-item-no-padding sClass='lenses-compare__net-chema net-chema'>
-          <div class="_forest_net">
-            <div class="camera-pair__row"></div>
-            <div class="camera-pair__row"></div>
+
+          <div class="lenses-compare__own-ts _forest_net">
+            <x-camera-watch-pair></x-camera-watch-pair>
+            <x-camera-watch-pair></x-camera-watch-pair>
           </div>
-          <div class="net-chema__title">Сеть профессиональных специализированных телесистемы ВПК</div>
-        </x-item-no-padding>
-        
-        {{-- cameras 1 --}}
-        <x-item-no-padding sClass='lenses-compare__net-chema net-chema'>
-          <div class="_forest_net">
-            <div class="camera-pair__row"></div>
-            <div class="camera-pair__row"></div>
+
+        {{-- cameras 2 --}}
+     
+          <div class="lenses-compare__casual-ts _forest_net ">
+            <x-camera-watch></x-camera-watch>
+            <x-camera-watch-pair></x-camera-watch-pair>
+            <x-camera-watch-pair></x-camera-watch-pair>
+            <x-camera-watch-pair></x-camera-watch-pair>
+            <x-camera-watch-pair></x-camera-watch-pair>
           </div>
-          <div class="net-chema__title">Сеть телесистем бытового назначения</div>
-        </x-item-no-padding>
-      </x-row>
-      
-      <x-row sClass='lenses-compare__net-params'>
+          
+
         
-        <x-item-no-padding sClass='lenses-compare__net-params-title'>Эффективная дальность осмотра</x-item-no-padding>
-        <x-item-no-padding>
-          <x-row>
-            <x-item-no-padding>30 км и дальше</x-item-no-padding>
-            <x-item-no-padding>до 10 км</x-item-no-padding>
-          </x-row>
-        </x-item-no-padding>
+        {{-- titles --}}
+        <div class="net-schema-title">Сеть профессиональных специализированных телесистемы ВПК</div>
+        <div class="net-schema-title">Сеть телесистем бытового назначения</div>
         
-        <x-item-no-padding sClass='lenses-compare__net-params-title'>Количество телекамер</x-item-no-padding>
-        <x-item-no-padding>
-          <x-row>
-            <x-item-no-padding>7</x-item-no-padding>
-            <x-item-no-padding>15</x-item-no-padding>
-          </x-row>
-        </x-item-no-padding>
+        {{-- parameter 1 --}}
+        <div class="grid-unite-row-cells table-header">Эффективная дальность осмотра</div>
+        <div>30 км и дальше</div>
+        <div>до 10 км</div>
         
-        <x-item-no-padding sClass='lenses-compare__net-params-title'>Количество рабочих мест</x-item-no-padding>
-        <x-item-no-padding>
-          <x-row>
-            <x-item-no-padding>2</x-item-no-padding>
-            <x-item-no-padding>4</x-item-no-padding>
-          </x-row>
-        </x-item-no-padding>
-      </x-row>
+        {{-- parameter 2 --}}
+        <div class="grid-unite-row-cells table-header">Количество телекамер</div>
+        <div>7</div>
+        <div>15</div>
+        
+        {{-- parameter 3 --}}
+        <div class="grid-unite-row-cells table-header">Количество рабочих мест</div>
+        <div>2</div>
+        <div>4</div>
+      </x-grid2>
       
     </x-item>
     
     <x-item-center>
+      <div class="list-big-number">
+        <div class="list-big-number__bg">02</div>
         <p>Собственно является логическим продолжением первого аргумента - количество телекамер, которые нужно установить для полного контроля над всей площадью леса. 
         Чем дальше видит объектив тем</p>
         <ul class="first-letter">
           <li>меньшее количество телесистем нужно устанавливать</li>
           <li>меньше затрат на обслуживание оборудования, в том числе и профилактических действий</li>
           <li>меньше операторов на пунктах наблюдения, как утверждают специалисты один человек может эффективно контролировать лес на изображениях 4х камер</li>
-        </ul>
+        </ul>        
+      </div>
+
     </x-item-center>
   </x-row>
     
